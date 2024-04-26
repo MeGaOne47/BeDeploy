b68eaac6aa3ab4c01abf72a0d990f628eb581907
Nguyễn Tấn Hùng <nguyentanhung9a1@gmail.com>
2024-04-26 16:35:12 +0700

    Update: env PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // Sử dụng middleware CORS
 app.use(cors())
 
-app.listen(3001, () => {
+app.listen(process.env.ACCESS_SECRET, () => {
     console.log('Server is running on port 3001');
 });
 
